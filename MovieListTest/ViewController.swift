bca015a237d0e9907ae0320351963488438b94e5
SandyDC <sandydchandra@gmail.com>
2020-09-03 16:35:20 +0700

    Initial Commit

---
+++ MovieListTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieListTest
+//
+//  Created by Sandy Chandra on 03/09/20.
+//  Copyright © 2020 Sandy Chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
